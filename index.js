48fb8fae15e4c90e680426dddb586f4dd5c58b6d
mtmak1 <unium.orb.4@gmail.com>
2020-10-18 14:21:16 +0500

    egor

---
+++ index.js
@@ -0,0 +1,7 @@
+
+var name = "Егор";
+alert(`Привет  ${name}`);
+
+
+alert(`Привет  ${name}`);alert(`Привет  ${name}`);alert(`Привет  ${name}`);alert(`Привет  ${name}`);
+document.querySelector("body").setAttribute("class",'dark')
